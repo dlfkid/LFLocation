3d1bf157d0a76da94a814cce50ee8ce0764e4525
dlfkid <dlfkid@icloud.com>
2019-08-26 18:57:20 +0800

    initial commit

---
+++ LFLocationDemo/LFLocationDemo/LFLocation/LFLocation.swift
@@ -0,0 +1,19 @@
+//
+//  LFLocation.swift
+//  LFLocationDemo
+//
+//  Created by LeonDeng on 2019/8/26.
+//  Copyright © 2019 LeonDeng. All rights reserved.
+//
+
+import CoreLocation
+
+class LFLocation: NSObject {
+    private let locationManager: CLLocationManager = CLLocationManager()
+    
+    
+}
+
+extension LFLocation: CLLocationManagerDelegate {
+    
+}
